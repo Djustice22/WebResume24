ad6419932ae1a9bf9eb44c36d78804cce1b3d625
Denise Justice <98673328+Djustice22@users.noreply.github.com>
2024-07-17 10:01:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+import "./Styles/main.scss";
+import "./Styles/media-queries.scss";
+import "./Styles/certificates.scss";
+import "./Styles/navigation.scss";
+import "./Styles/about.scss";
+import "./Styles/education.scss";
+import "./Styles/experience.scss";
+import "./Styles/skills_languages.scss";
+import "./Styles/under-construction.scss";
+import "./Styles/footer.scss";
